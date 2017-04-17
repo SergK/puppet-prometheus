25cac20d35ee1eb0d2ea72312dad4c3cc0f1aa5c
Sergey Kulanov <sergey@kulanov.org.ua>
2017-04-18 00:07:28 +0300

    Add configuration files

--- manifests/config.pp
+++ manifests/config.pp
@@ -11,12 +11,26 @@ class prometheus::config (
     mode   => '0755',
   }
 
+  # add push_gw monitoring
+  file { "${config_path}/prometheus.yml":
+    content => template('prometheus/prometheus.yml.erb'),
+    require => File[$config_path],
+    mode    => '0644',
+  }
+
   # ensure we have directory for file_sd_configs
   # https://prometheus.io/docs/operating/configuration/#%3Cfile_sd_config%3E
   file { $file_sd_config_path:
     ensure  => 'directory',
     mode    => '0755',
     require => File[$config_path],
+  }
+
+  # add push_gw monitoring
+  file { "${file_sd_config_path}/push_gateway.yml":
+    content => template('prometheus/push_gateway.yml.erb'),
+    require => File[$file_sd_config_path],
+    mode    => '0644',
   }
 
 }
