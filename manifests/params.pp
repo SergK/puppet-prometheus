5fa615f368bdc95796eabb1828775be58510b926
Sergey Kulanov <sergey@kulanov.org.ua>
2017-04-18 00:51:34 +0300

    Ensure we manage prometheus service

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,16 +3,16 @@
 class prometheus::params {
   $config_dir                  = '/etc/prometheus'
   $file_sd_config_dir          = "${config_dir}/file_sd_configs"
-  $jmx_agent_package_name      = 'prometheus-jmx-agent'
   $jmx_agent_package_ensure    = 'latest'
-  $package_name                = 'prometheus'
+  $jmx_agent_package_name      = 'prometheus-jmx-agent'
   $package_ensure              = 'latest'
-  $push_gateway_package_name   = 'prometheus-pushgateway'
-  $push_gateway_package_ensure = 'latest'
+  $package_name                = 'prometheus'
   $push_gateway_enabled        = true
+  $push_gateway_package_ensure = 'latest'
+  $push_gateway_package_name   = 'prometheus-pushgateway'
+  $service_enable              = true
+  $service_ensure              = 'running'
+  $service_manage              = true
   $storage_path                = '/var/lib/prometheus'
-
-  $service_enable = true
-  $service_ensure = 'running'
 
 }
